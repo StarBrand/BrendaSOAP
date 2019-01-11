8187f7d7ec59f231650c075ceb095a1effa82a3b
StarBrand <juan.saez.hidalgo@gmail.com>
2019-01-11 19:29:17 -0300

    hopefully nothing

--- src/client/DefaultUser.java
+++ src/client/DefaultUser.java
@@ -3,7 +3,7 @@ package client;
 public class DefaultUser extends User{
 
   public DefaultUser() {
-    super("jdoe@something.com", "password1234");
+    super("jdoe@something.com", "password123");
   }
 
 }
