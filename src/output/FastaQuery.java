75aba479b6f799058ca3b96c24aaca94798cf1d4
StarBrand <juan.saez.hidalgo@gmail.com>
2019-02-02 21:45:58 -0300

    Fixed a problem with 'generateFile' method, tested and checked with UniProt

--- src/output/FastaQuery.java
+++ src/output/FastaQuery.java
@@ -1,18 +1,16 @@
 package output;
 
-import attributes.Attribute;
 import attributes.enzyme_structure.AASequence;
-import client.SoapClient;
 import client.User;
 import entities.Entity;
 import entities.Protein;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import queries.ParserAnswer;
-import queries.Query;
 
 /**
  * FastaQuery class, generate a .txt file
@@ -20,16 +18,12 @@ import queries.Query;
  *
  * @author Juan Saez Hidalgo
  */
-public class FastaQuery implements Query {
+public class FastaQuery extends EnzymeStructureQuery {
 
-  private SoapClient client;
-  private User user;
-  private List<Entity> proteins;
-  private List<Attribute> attributes;
   private AASequence aaSequence;
-  private ParserAnswer parserAnswer;
+  private List<Entity> proteins;
   private List<AASequence> result;
-
+  private ParserAnswer parserAnswer;
 
   /**
    * FastaQuery constructor given a Brenda User
@@ -37,14 +31,12 @@ public class FastaQuery implements Query {
    * @param user Brenda User
    */
   public FastaQuery(User user){
-    this.user = user;
-    client = new SoapClient(user);
-    proteins = new ArrayList<Entity>();
-    attributes = new ArrayList<Attribute>();
+    super(user);
     aaSequence = new AASequence();
+    proteins = new ArrayList<Entity>();
+    result = new ArrayList<AASequence>();
     parserAnswer = new ParserAnswer();
   }
-
 
   /**
    * Set the list of {@Link entities.Protein} to get
@@ -58,51 +50,34 @@ public class FastaQuery implements Query {
     }
   }
 
-  public void addAttributes(Attribute... attribute) {
-
-  }
-
-  public int numberOfAttributes() {
-    return 0;
-  }
-
   public List<?> getResult() throws Exception {
-    String result;
-    client.makeCall();
-    this.result = new ArrayList<AASequence>();
+    this.result.clear();
     List<Entity> new_proteins = new ArrayList<Entity>();
-    for (Entity protein:proteins){
-      if(((Protein) protein).getUniprot().equals("")){
-        AASequence aaSequence = new AASequence();
-        result = "";
-      }
-      else {
-        String[] uniprots = ((Protein) protein).getUniprot().split(" AND ");
-        for (String uniprot : uniprots) {
+    for (Entity protein:proteins) {
+      String[] uniprots = ((Protein) protein).getUniprot().split(" AND ");
+      for (String uniprot : uniprots) {
+        if(!uniprot.equals("")) {
           Protein aProtein = new Protein(
               ((Protein) protein).getEnzyme(),
               ((Protein) protein).getOrganism(),
               uniprot
           );
           new_proteins.add(aProtein);
-          AASequence aaSequence = new AASequence();
-          result = client.getResult(
-              ((Protein) protein).getEnzyme().getParameter() +
-                  "#firstAccessionCode*" +
-                  uniprot
-              , aaSequence.getMethod());
-          if (result.equals("")) {
-            this.result.add(aaSequence);
-          } else {
-            aaSequence.setAttribute(
-                parserAnswer.getResult(result).get(0)
-            );
-            this.result.add(aaSequence);
-          }
         }
       }
     }
-    this.proteins = new_proteins;
+    proteins = new_proteins;
+    List<String> result = super.getResult(false, true, aaSequence, proteins);
+    for(String ans:result){
+      AASequence aaSequence = new AASequence();
+      if(ans.equals("")) this.result.add(aaSequence);
+      else{
+        aaSequence.setAttribute(
+            parserAnswer.getResult(ans).get(0)
+        );
+        this.result.add(aaSequence);
+      }
+    }
     return this.result;
   }
 
@@ -114,39 +89,64 @@ public class FastaQuery implements Query {
    * @see attributes.enzyme_structure.AASequence
    */
   public void generateFile() throws IOException {
-    String line = "";
+    List<String> line = new ArrayList<String>();
+    List<String> line2 = new ArrayList<String>();
+    String tempLine;
     int lines, rest;
-    FileWriter file = new FileWriter("fasta_output.txt");
-    BufferedWriter bf = new BufferedWriter(file);
+    (new File(user.getMail() + "_results")).mkdirs();
+    FileWriter file = new FileWriter(user.getMail() + "_results\\fasta_output.txt");
+    FileWriter file2 = new FileWriter(user.getMail() + "_results\\report_fasta.txt");
+    BufferedWriter bw = new BufferedWriter(file);
+    BufferedWriter bw2 = new BufferedWriter(file2);
+    line2.add("UniProt");
+    line2.add("Enzyme");
+    line2.add("Organism");
+    line2.add("Source");
+    line2.add("Found_using_Brenda");
+    TableWriter.write(line2, "\t", bw2);
     for (int i = 0; i < result.size(); i++) {
       if (((Protein) proteins.get(i)).getUniprot() == "") {
-
       } else {
         lines = result.get(i).getNumberOfAminoacids() / 80;
         rest = result.get(i).getNumberOfAminoacids() % 80;
-        line = ">";
-        line += ((Protein) proteins.get(i)).getUniprot() + " ";
-        line += ((Protein) proteins.get(i)).getEnzyme().getRecommended_name() + " ";
-        line += ((Protein) proteins.get(i)).getOrganism().getName();
-        bf.write(line);
-        bf.newLine();
-        line = "";
+        line.clear();
+        line2.clear();
+        tempLine = ((Protein) proteins.get(i)).getUniprot();
+        line.add(">" + tempLine);
+        line2.add(tempLine);
+        tempLine = ((Protein) proteins.get(i)).getEnzyme().getRecommended_name();
+        line.add(tempLine);
+        line2.add(tempLine);
+        tempLine = ((Protein) proteins.get(i)).getOrganism().getName();
+        line.add(tempLine);
+        line2.add(tempLine);
+        tempLine = result.get(i).getSource();
+        line2.add(tempLine);
+        if(result.get(i).getNumberOfAminoacids() == 0){
+          line2.add("FALSE");
+        }
+        else{
+          line2.add("TRUE");
+          line.add("Source: Brenda->"+tempLine);
+        }
+        TableWriter.write(line, "|", bw);
+        TableWriter.write(line2, "\t", bw2);
+        line.clear();
         for (int j = 0; j < lines; j++) {
-          line += result.get(i).getSequence().substring(j * 80, (j * 80) + 79);
-          bf.write(line);
-          bf.newLine();
-          line = "";
+          line.add(result.get(i).getSequence().substring(j * 80, (j+1) * 80 ));
+          TableWriter.write(line, "\t", bw);
+          line.clear();
         }
         if (rest != 0) {
-          line += result.get(i).getSequence().substring(lines * 80, lines * 80 + rest - 1);
-          bf.write(line);
-          bf.newLine();
-          line = "";
+          line.add(result.get(i).getSequence().substring(lines * 80, lines * 80 + rest));
+          TableWriter.write(line, "\t", bw);
+          line.clear();
         }
-        bf.newLine();
-        bf.newLine();
+        bw.newLine();
+        bw.newLine();
       }
     }
-    bf.close();
+    bw.close();
+    bw2.close();
   }
 }
