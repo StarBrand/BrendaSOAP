cdce48c93e1b7940ab5d5bca0291fd4b57e81f29
StarBrand <juan.saez.hidalgo@gmail.com>
2019-02-02 21:48:46 -0300

    Change the output from one table to referenced tables

--- src/output/OutputTable.java
+++ src/output/OutputTable.java
@@ -3,13 +3,16 @@ package output;
 import static java.lang.StrictMath.max;
 
 import attributes.Attribute;
+import client.User;
 import entities.Protein;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import javafx.scene.control.Tab;
 
 /**
  * output.OutputTable class, generates the table in csv format
@@ -24,24 +27,31 @@ public class OutputTable {
 
   private List<Protein> proteins;
   private List<String> data;
-  private List<String> columns;
-  private List<HashMap<String, String>> rows;
+  private List<String> protein_columns;
+  private HashMap<String, List<String>> attributes_columns;
+  private List<HashMap<String, String>> protein_rows;
+  private HashMap<String, List <HashMap<String, String>>> attributes_rows;
+  private User user;
 
   /**
    * Constructor
    */
-  public OutputTable(){
+  public OutputTable(User user){
     proteins = new ArrayList<Protein>();
     data = new ArrayList<String>();
-    columns = new ArrayList<String>();
-    rows = new ArrayList<HashMap<String, String>>();
-    columns.add("EC Number");
-    columns.add("Systematic Name");
-    columns.add("Recommended Name");
-    columns.add("UniProt");
-    columns.add("Organism");
-    columns.add("Organism Commentary");
-    columns.add("Organism Literature (Pubmed ID)");
+    protein_columns = new ArrayList<String>();
+    attributes_columns = new HashMap<String, List<String>>();
+    protein_rows = new ArrayList<HashMap<String, String>>();
+    attributes_rows = new HashMap<String, List<HashMap<String, String>>>();
+    protein_columns.add("EC_Number");
+    protein_columns.add("Systematic_name");
+    protein_columns.add("Recommended_name");
+    protein_columns.add("UniProt");
+    protein_columns.add("Organism");
+    protein_columns.add("Commentary");
+    protein_columns.add("Literature(PubmedID)");
+    protein_columns.add("Ref");
+    this.user = user;
   }
 
   /**
@@ -93,81 +103,102 @@ public class OutputTable {
   public void generateRows() {
     for (Protein protein : proteins) {
       HashMap<String, String> row = new HashMap<String, String>();
-      row.put(columns.get(0), protein.getEnzyme().getEC().toString());
-      row.put(columns.get(1), protein.getEnzyme().getSystematic_name());
-      row.put(columns.get(2), protein.getEnzyme().getRecommended_name());
-      row.put(columns.get(3), protein.getUniprot());
+      row.put(protein_columns.get(0), protein.getEnzyme().getEC().toString());
+      row.put(protein_columns.get(1), protein.getEnzyme().getSystematic_name());
+      row.put(protein_columns.get(2), protein.getEnzyme().getRecommended_name());
+      row.put(protein_columns.get(3), protein.getUniprot());
       HashMap<String, String> organism = protein.getOrganism().rowsToTable();
-      row.put(columns.get(4), organism.get(columns.get(4)));
-      row.put(columns.get(5), organism.get(columns.get(5)));
-      row.put(columns.get(6), organism.get(columns.get(6)));
+      row.put(protein_columns.get(4), organism.get(protein_columns.get(4)));
+      row.put(protein_columns.get(5), organism.get(protein_columns.get(5)));
+      row.put(protein_columns.get(6), organism.get(protein_columns.get(6)));
+      String ref = String.valueOf(proteins.indexOf(protein));
+      row.put(protein_columns.get(7), ref);
       for (Attribute attribute : protein.getAttribute()) {
-        try {
-          HashMap<String, String> new_rows = attribute.rowsToTable();
-          if (data.contains(attribute.getAttributeName())) {
-            if (row.keySet().contains(attribute.getAttributeName() + " value")) {
-              for (String column : new_rows.keySet()) {
-                try {
-                  String value = row.get(column);
-                  value += "; " + new_rows.get(column);
-                  row.put(column, value);
-                } catch (Exception e) {
-                  row.put(column, "");
-                }
+        String name = attribute.getAttributeName();
+        if (data.contains(name)){
+          try {
+            HashMap<String, String> new_row = attribute.rowsToTable();
+            new_row.put("Ref", ref);
+            if (!attributes_columns.keySet().contains(name)) {
+              List<String> columns = new ArrayList<String>();
+              for (String column : new_row.keySet()) {
+                columns.add(column);
               }
-            } else {
-              for (String column : new_rows.keySet()) {
-                row.put(column, new_rows.get(column));
-                if(!columns.contains(column)){
-                  columns.add(column);
-                }
-              }
+              attributes_columns.put(name, columns);
             }
+            new_row.put("Attribute", name);
+            if(attributes_rows.containsKey(name)){
+              attributes_rows.get(name).add(new_row);
+            }
+            else{
+              List<HashMap<String, String>> new_rows = new ArrayList<HashMap<String, String>>();
+              new_rows.add(new_row);
+              attributes_rows.put(name, new_rows);
+            }
+          } catch (NullPointerException e) {
           }
-        } catch (NullPointerException e){}
+        }
       }
-      rows.add(row);
+      protein_rows.add(row);
     }
   }
 
-  public void out() throws IOException{
-    String line, header = "";
-    FileWriter file = new FileWriter("table.txt");
-    BufferedWriter bf = new BufferedWriter(file);
-    for(String column:columns){
-      header += column + "\t";
+  public void proteins_out() throws IOException{
+    (new File(user.getMail() + "_results")).mkdirs();
+    out(user.getMail() + "_results\\table.txt", protein_columns, protein_rows);
+  }
+
+  public void attributes_out() throws IOException {
+    (new File(user.getMail() + "_results\\attributes")).mkdirs();
+    for(String name:attributes_columns.keySet()) {
+      String file_name = user.getMail() + "_results\\attributes\\";
+      out(file_name + name +".txt", attributes_columns.get(name), attributes_rows.get(name));
     }
-    header = header.substring(0, max(header.length() - 1, 0));
-    bf.write(header);
-    bf.newLine();
-    for(HashMap<String, String> row:rows){
-      line = "";
-      for(String column:header.split("\t")){
+  }
+
+  private void out(String name, List<String> head_line, List<HashMap<String, String>> row_lines)  throws IOException {
+    FileWriter file = new FileWriter(name);
+    BufferedWriter bw = new BufferedWriter(file);
+    List<String> header = new ArrayList<String>();
+    List<String> line = new ArrayList<String>();
+    for(String column:head_line){
+      header.add(column);
+    }
+    TableWriter.write(header, "\t", bw);
+    for(HashMap<String, String> row:row_lines){
+      line.clear();
+      for(String column:header){
         try {
-          line += row.get(column) + "\t";
+          line.add(row.get(column));
         } catch (Exception e){
-          line += "NA" + "\t";
+          line.add("NA");
         }
       }
-      line = line.substring(0, max(line.length() - 1, 0));
-      bf.write(line);
-      bf.newLine();
+      TableWriter.write(line, "\t", bw);
     }
-    bf.close();
+    bw.close();
   }
 
   public void deleteLiteratureColumn(){
-    for(HashMap<String, String> row:rows){
-      row.remove(columns.get(6));
+    for(HashMap<String, String> row:protein_rows){
+      row.remove(protein_columns.get(6));
     }
-    columns.remove(6);
+    protein_columns.remove(6);
   }
 
-  public List<HashMap<String, String>> getRows() {
-    return rows;
+  public List<String> getProtein_columns() {
+    return protein_columns;
   }
 
-  public List<String> getColumns() {
-    return columns;
+  public List<HashMap<String, String>> getProtein_rows() {
+    return protein_rows;
+  }
+
+  public HashMap<String, List<String>> getAttributes_columns() {
+    return attributes_columns;
+  }
+
+  public HashMap<String, List<HashMap<String, String>>> getAttributes_rows() {
+    return attributes_rows;
   }
 }
